b0b14016c026353d8276c24f70df9175b05fa858
karavias <dim1988g@hotmail.com>
2016-04-02 14:21:53 +0200

    Update lesson and collision lesson and exercise 10

---
+++ Assets/Scripts/Lesson4/Character.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    public float force = 10f;
+    public float speed = 1f;
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up * force);
+        }
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+        //if (Input.GetAxis("Horizontal"))
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        print("Collided with : " + col.gameObject.name);
+        if (col.gameObject.tag == "star")
+        {
+            //col.gameObject.GetComponent<MyScript>().points;
+            Destroy(col.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Lesson4/DigitalWatch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DigitalWatch : MonoBehaviour {
+
+
+    public TextMesh textMesh;
+
+    float seconds = 0f;
+
+	// Update is called once per frame
+	void Update () {
+        seconds += Time.deltaTime;
+        textMesh.text = (int)(seconds / 60) + ":" + (int)(seconds % 60);
+	}
+}
